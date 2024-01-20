2f54663aba54e714353cf235bb530ce0ab884d9a
CundullahT-CT <cterzioglu@cybertekschool.com>
2024-01-20 19:31:19 +0100

    fixes keycloak + swagger + cors

---
+++ src/main/java/com/cydeo/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.cydeo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${KEYCLOAK}")
+    private String keycloakIP;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://" + keycloakIP + ":8080"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Content-Type", "Authorization"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
+}
---
+++ src/main/java/com/cydeo/config/OpenAPI3Configuration.java
@@ -0,0 +1,23 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Component
+public class OpenAPI3Configuration {
+
+    @Bean
+    public OpenAPI gatewayOpenApi() {
+        return new OpenAPI().info(new Info().title("Ticketing Application Microservices API Docs ")
+                .description("Swagger Documentations for all the Microservices in Ticketing Application")
+                .version("v1.0")
+                .contact(new Contact()
+                        .name("Ticketing Application Development Team")
+                        .email("support@ticketingappcontact.com")));
+    }
+
+}
